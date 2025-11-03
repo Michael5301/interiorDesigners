96310b1e21bd750e4306ceda178b60e595887686
michael <maikogagi1@gmail.com>
2025-11-03 18:38:42 +0100

    desploy update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
-import React from 'react'
 import Hero from '../components/Hero'
-import Services from '../components/services'
+import Services from '../components/Services'
 import Team from '../components/Team'
 
 
